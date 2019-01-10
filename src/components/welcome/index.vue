ff5ba47921cf8045ac8d442c07b24b893dff3300
ouzuyu <oupenguin@gmail.com>
2019-01-11 00:25:18 +0800

    增加歡迎頁

--- src/components/welcome/index.vue
+++ src/components/welcome/index.vue
@@ -1,12 +1,70 @@
 <template>
-    <div class="welcome">
-        <div></div>
+    <div class="welcome" v-if="isWelcomeShow">
+        <h3 class="title">welcome</h3>
+
+        <div class="emoji">
+            <div class="eyes">
+                <span class="left-eye">^</span>
+                <span class="right-eye">^</span>
+            </div>
+            <div class="mouth"></div>
+        </div>
+
+        <div class="date">{{ currentDate }}</div>
+        <div class="location">您位于：{{myCity}}</div>
+        <div class="douban-logo-wrap">
+            <img class="logo" src="./douban-logo.png" alt="logo">
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+    computed: {
+        ...mapState({
+            myCity: state => state.location.myCity,
+        }),
+
+        currentDate () {
+            let date = new Date,
+                y = date.getFullYear(),
+                m = date.getMonth() + 1,
+                d = date.getDate(),
+                h = date.getHours(),
+                M = date.getMinutes(),
+                s = date.getSeconds();
+            return `${y}年${m}月${d}日 ${h}:${M}:${d}`;
+        },
+
+        isWelcomeShow () {
+            console.log(this.second)
+            return this.second !== 0;
+        }
+    },
+
+    data () {
+        return {
+            second: 3
+        }
+    },
+
+    methods: {
+        autoHideWelcome () {
+            let timer = setInterval(() => {
+                if (this.second === 0) {
+                    clearInterval(timer);
+                    return;
+                }
+                this.second--;
+            }, 1000);
+        }
+    },
+
+    created () {
+        this.autoHideWelcome();
+    }
 }
 </script>
 
@@ -17,7 +75,50 @@ export default {
     top: 0;
     bottom: 0;
     right: 0;
-    z-index: 9999;
-    background: #f9f9f9;
+    background: #fff;
+    text-align: center;
+
+    .title {
+        margin: 120px 0;
+        font-size: 50px;
+        color: #42bd56;
+    }
+}
+
+.emoji {
+    font-size: 100px;
+    color: #42bd56;
+
+    .eyes {
+        text-align: center;
+        .left-eye, .right-eye {
+            display: inline-block;
+            width: 100px;
+        }
+    }
+
+    .mouth {
+        width: 40px;
+        height: 6px;
+        background: #42bd56;
+        margin: 4px auto 0;
+    }
+}
+
+.date, .location {
+    margin-top: 40px;
+    font-size: 30px;
+    color: #42bd56;
+}
+
+.douban-logo-wrap {
+    position: fixed;
+    left: 50%;
+    bottom: 100px;
+    transform: translateX(-50%);
+
+    .logo {
+        width: 130px;
+    }
 }
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,18 +2,15 @@
     <div>
         <appmain></appmain>
         <tabs></tabs>
-
-        <welcome></welcome>
     </div>
 </template>
 
 <script>
 import appmain from './components/AppMain';
 import tabs from './components/Tabs';
-import welcome from '@/components/welcome';
 
 export default {
-    components: { appmain, tabs, welcome }
+    components: { appmain, tabs }
 }
 </script>
 
