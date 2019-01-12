8bad18be43440a141275d7f2650bc15ba75ee86a
ouzuyu <oupenguin@gmail.com>
2019-01-12 09:53:46 +0800

    增加请求loading

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -1,5 +1,18 @@
 const common = {
     state: {
+        requestLoading: 0
+    },
+
+    mutations: {
+        SET_LOADING: (state, boolean) => {
+            boolean ? ++state.requestLoading : --state.requestLoading
+        },
+    },
+
+    actions: {
+        SetLoading({ commit }, boolean) {
+            commit('SET_LOADING', boolean)
+        },
     }
 }
 
---
+++ src/utils/request.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import store from '../store'
+
+const service = axios.create({
+    baseURL: '/db',
+    timeout: 5000
+})
+
+// 请求拦截
+service.interceptors.request.use(
+    config => {
+
+        // 开启请求loading
+        store.dispatch('SetLoading', true);
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    }
+)
+
+// 响应拦截
+service.interceptors.response.use(
+    response => {
+
+        // 关闭请求loading
+        store.dispatch('SetLoading', false);
+        return response.data
+    },
+    error => {
+        console.log('err' + error)
+
+        // 关闭请求loading
+        store.dispatch('SetLoading', false)
+        return Promise.reject(error)
+    }
+)
+
+export default service
