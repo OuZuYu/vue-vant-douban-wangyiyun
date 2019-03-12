af9d242a2fc13bb51cec0754d005ebfb42f42d43
ouzuyu <oupenguin@gmail.com>
2019-03-12 23:58:18 +0800

    轮流使用百度、搜狐定位

--- src/store/modules/location.js
+++ src/store/modules/location.js
@@ -1,11 +1,49 @@
 const SOHU_GETCITY_API = 'http://pv.sohu.com/cityjson?ie=utf-8';
 const SCRIPT_ID = 'location';
 
+function locationBySouhu (commit, state) {
+    let oldScript = document.getElementById(SCRIPT_ID);
+    let body = document.getElementsByTagName('body')[0];
+    let script = document.createElement('script');
+
+    if (oldScript) body.removeChild(oldScript);
+
+    script.id = SCRIPT_ID;
+    script.type = 'text/javascript';
+    script.src = SOHU_GETCITY_API + `&t=${Date.now()}`;
+    body.appendChild(script);
+
+    return new Promise((resolve, reject) => {
+        script.onload = () => {
+            if (returnCitySN && returnCitySN.cname && returnCitySN.cname !== '国内未能识别的地区') {
+                console.log('搜狐定位:' + returnCitySN.cname)
+                let city = returnCitySN.cname.match(/.+?(省|市|自治区|自治州|县|区)/g);
+                let index = city.findIndex(val => val.indexOf('市') !== -1); // 找到第一个市
+                commit('SET_CITY', city[index]);
+                resolve(returnCitySN);
+            }
+        }
+    });
+}
+
+function locationByBaidu (commit, state) {
+    let city = new BMap.LocalCity();
+    return new Promise((resolve, reject) => {
+        city.get(res => {
+            console.log('百度定位:' + res.name)
+            commit('SET_CITY', res.name);
+            resolve(res);
+        })
+    });
+}
+
+
 const location = {
     state: {
         longitude: '',
         latitude: '',
-        myCity: '定位中...'
+        myCity: '定位中...',
+        flag: true
     },
 
     mutations: {
@@ -16,35 +54,11 @@ const location = {
 
     actions: {
         GetCity({ commit, state }) {
-            let oldScript = document.getElementById(SCRIPT_ID);
-            let body = document.getElementsByTagName('body')[0];
-            let script = document.createElement('script');
 
-            if (oldScript) body.removeChild(oldScript);
-
-            script.id = SCRIPT_ID;
-            script.type = 'text/javascript';
-            script.src = SOHU_GETCITY_API + `&t=${Date.now()}`;
-            body.appendChild(script);
-
-            return new Promise((resolve, reject) => {
-                script.onload = () => {
-                    let city;
-                    if (returnCitySN && returnCitySN.cname && returnCitySN.cname !== '国内未能识别的地区') {
-                        city = returnCitySN.cname.match(/.+?(省|市|自治区|自治州|县|区)/g);
-                        let index = city.findIndex(val => val.indexOf('市') !== -1); // 找到第一个市
-                        commit('SET_CITY', city[index]);
-                        resolve(returnCitySN);
-                    } else {
-                        city = new BMap.LocalCity();
-                        city.get(res => {
-                            console.log(res)
-                            commit('SET_CITY', res.name);
-                            resolve(res);
-                        })
-                    }
-                }
-            });
+            // 轮流使用搜狐或百度定位
+            let result = state.flag ? locationBySouhu(commit, state) : locationByBaidu(commit, state);
+            state.flag = !state.flag;
+            return result;
         },
     }
 }
